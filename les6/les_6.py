fe34e7917037ad59f938c0b7f2b4592c17c1cd7c
D.Wimmers <daanrw@hotmail.com>
2023-11-13 16:22:45 +0100

    les 6 deels af

---
+++ les6/les_6.py
@@ -0,0 +1,44 @@
+'''opdracht 1a'''
+auto_merken=["toyota","mazda","volkswagen","jeep"]
+print(auto_merken)
+
+'''opdracht 1b'''
+
+auto_merken=["toyota","mazda","volkswagen","jeep"]
+auto_merken.append("suzuki")
+print(auto_merken)
+
+
+'''opdracht 2a'''
+mijn_dictionary={
+    "voornaam" :"Harry",
+    "achternaam" : "van Winkel",
+    "geboortedatum" : "27-3-1939"}
+print(mijn_dictionary)
+
+'''opdracht 2b'''
+mijn_dictionary={
+    "voornaam" :"Harry",
+    "achternaam" : "van Winkel",
+    "geboortedatum" : "27-3-1939"}
+print(mijn_dictionary)
+
+
+
+'''opdracht 3'''
+a=10
+b=2
+c=a/b
+print(c)
+
+'''opdracht 4'''
+a=2
+b=3
+c=a**b
+print(c)
+
+'''opdracht 5'''
+a=4
+b=5
+c=a==b
+print(c)
