20a59a2d5f4f37859f46c3b47e26a10d406a3097
D.Wimmers <daanrw@hotmail.com>
2023-11-13 10:55:21 +0100

    dit is de eerste versie

---
+++ les3hw2.py
@@ -0,0 +1,7 @@
+txt=("Hallo Europa")
+print(txt)
+
+
+#opdracht 3
+tvt=("door txt eerst een fuctie te geven, in dit geval hallo europa, print je de teskt als je vraagt om txt te printen")
+print(tvt)
