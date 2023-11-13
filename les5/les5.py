4e139eac7f2674c5b0704609be85ac87157d844f
D.Wimmers <daanrw@hotmail.com>
2023-11-13 13:53:27 +0100

    les 5 is af en kan worden ingeleverd

---
+++ les5/les5.py
@@ -0,0 +1,21 @@
+'''opgave 1'''
+Kip=10
+ei= 0.5
+totaal= Kip + ei
+print(totaal)
+
+'''opgave 2'''
+i=10
+j=2
+y=20
+while i < 20:
+    print(int(i),"maal 2 is", int(y)) 
+    i=i+1
+    y=j*i
+
+
+'''opgave 3'''
+leeftijd=30
+naam ="Deborah"
+
+print("Haar naam is",naam + ",", "ze is", leeftijd,"jaar oud.")
